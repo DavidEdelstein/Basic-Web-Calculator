c79d16db100e3147063cbea87c7a6f9f84e73819
David Edelstein <yitzedelstein@gmail.com>
2019-01-17 00:48:36 -0500

    basic controller and model/working on view

---
+++ BasicWebCalculator/BasicWebCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,42 @@
+﻿using BasicWebCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BasicWebCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CalculatorModel model, string command)
+        {
+            if (command=="add")
+            {
+                model.Result = model.A + model.B;
+            }
+            if (command=="subtraction")
+            {
+                model.Result = model.A - model.B;
+            }
+            if (command=="multiplication")
+            {
+                model.Result = model.A * model.B;
+            }
+            if (command=="division")
+            {
+                model.Result = model.A / model.B;
+            }
+            return View(model);
+        }
+
+
+    }
+}
---
+++ BasicWebCalculator/BasicWebCalculator/Models/CalculatorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicWebCalculator.Models
+{
+    public class CalculatorModel
+    {
+        //public int A { get; set; }
+        //public int B { get; set; }
+        //public int Result { get; set; }
+
+        public double A { get; set; }
+        public double B { get; set; }
+        public double Result { get; set; }
+
+
+    }
+}
